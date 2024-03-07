fd518252537d1ce5fe702794def30dab5836647d
CruzPort <125126639+CruzPort@users.noreply.github.com>
2024-03-07 22:55:27 +0530

    Updated

---
+++ Day 67/Middle of the linked list.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int getLenght (ListNode* head)
+    {
+        int size = 0;
+        ListNode* curr = head;
+        while (curr != nullptr)
+        {
+            size++;
+            curr = curr->next;
+        }
+        return size;
+    }
+    ListNode* middleNode(ListNode* head) {
+        int size = getLenght(head);
+        int pos = size/2+1;
+        int curr_pos = 1;
+        ListNode* curr = head;
+        
+        //loop to get size
+        while (curr_pos != pos)
+        {
+            curr_pos++;
+            curr = curr->next;
+        }
+        
+        return curr;
+    }
+};
