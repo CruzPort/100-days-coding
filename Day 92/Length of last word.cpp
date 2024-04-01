567120c5cfd67127ed8bb987ad7d7837ff6492e9
CruzPort <125126639+CruzPort@users.noreply.github.com>
2024-04-01 23:46:10 +0530

    Updated

---
+++ Day 92/Length of last word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        stringstream ss(s);
+        string word;
+        int len = 0;
+
+        while (ss >> word) {
+            len = word.size(); 
+        }
+
+        return len;
+    }
+};
