6b3232a60a7096340b40c2cdb1167448b4d10977
CruzPort <125126639+CruzPort@users.noreply.github.com>
2024-05-08 23:37:05 +0530

    Updated

---
+++ Day 129/Relative ranks.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        int n = score.size();
+        map<int, int> mp1;
+        vector<string> result(n);
+        
+        for (int i = 0; i < n; i++) {
+            mp1[score[i]] = i;
+        }       
+        sort(score.begin(), score.end(), greater<int>());   
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                result[mp1[score[i]]] = "Gold Medal";
+            } else if (i == 1) {
+                result[mp1[score[i]]] = "Silver Medal";
+            } else if (i == 2) {
+                result[mp1[score[i]]] = "Bronze Medal";
+            } else {
+                result[mp1[score[i]]] = to_string(i + 1);
+            }
+        }
+        
+        return result;
+    }
+};
