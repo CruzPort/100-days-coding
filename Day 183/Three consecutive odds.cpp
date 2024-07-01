1ec06532dcf6d1ef93a4709e5631f172946d6180
CruzPort <125126639+CruzPort@users.noreply.github.com>
2024-07-01 23:29:37 +0530

    Updated

---
+++ Day 183/Three consecutive odds.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    static bool threeConsecutiveOdds(vector<int>& arr) {
+        int len=0;
+        for(int x: arr){
+            len=(x&1)?len+1:0;
+            if (len==3) return 1;
+        }
+        return 0;
+    }
+};
+
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
